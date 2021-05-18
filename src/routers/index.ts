8e7df47060b0bc02a1cbad77b9f0680d63e8b57c
keeping <starunaway@gmail.com>
2021-05-18 15:35:20 +0800

    feat: 🎸 update router && vite build config

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,4 +1,4 @@
-import {createRouter, createWebHistory} from 'vue-router';
+import {createRouter, createWebHashHistory} from 'vue-router';
 
 import Home from '@/pages/Home/index.vue';
 import About from '@/pages/About/index.vue';
@@ -10,7 +10,7 @@ const routes = [
 
 const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes, // `routes: routes` 的缩写
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,33 @@ import {defineConfig} from 'vite';
 import path from 'path';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
+import packageConf from './package.json';
+import copy from 'rollup-plugin-copy';
+
+const tarFileName = `${packageConf.name}`;
+const isDev = process.env.NODE_ENV === 'development';
+const rollupPlugins = [];
+
+if (!isDev) {
+  console.log('copy');
+  rollupPlugins.push(
+    copy({
+      targets: [{src: 'package.json', dest: `build/${tarFileName}`}],
+      verbose: true,
+      hook: 'writeBundle',
+    })
+  );
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), vueJsx()],
+  build: {
+    outDir: `build/${tarFileName}`,
+    rollupOptions: {
+      plugins: rollupPlugins,
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
